ac6453b630132c80977c1eac3e8a7f07caccc451
pierre paci <pierre.paci@gmail.com>
2017-06-05 19:24:00 +0200

    Created a TwitterService

--- src/main/kotlin/io/spidey/Controller/HelloWorldController.kt
+++ src/main/kotlin/io/spidey/Controller/HelloWorldController.kt
@@ -2,23 +2,26 @@ package io.spidey.Controller
 
 import io.reactivex.Flowable
 import io.reactivex.Single
+import io.spidey.Services.TwitterService
 import org.springframework.social.twitter.api.Tweet
 import org.springframework.social.twitter.api.impl.TwitterTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/")
-class HelloWorldController constructor() {
-    val twitter = TwitterTemplate("qPe0o5axovP4VlGnv9bDchVwI", "yFWHo5mHwIy99jlTfgp2loSpnRlGLMwRvpNJqqFC6NQQfqal15")
+class HelloWorldController constructor(val TwitterService: TwitterService) {
+
 
     @GetMapping
     fun hello(): Single<String> = Single.just("hello world").map { it.toUpperCase() }
 
     @GetMapping("/twitter")
-    fun hello_search(): Flowable<Tweet> {
-        return Flowable.fromIterable(this.twitter.searchOperations().search("hello world").tweets)
+    fun hello_search(@RequestParam("number", required = false) number:Int?): Flowable<Tweet> {
+        return this.TwitterService.getHelloWorldTweets(number = number ?: 1)
     }
 
 }
--- src/main/kotlin/io/spidey/Services/TwitterService.kt
+++ src/main/kotlin/io/spidey/Services/TwitterService.kt
@@ -1,7 +1,18 @@
 package io.spidey.Services
 
+import io.reactivex.Flowable
+import org.springframework.social.twitter.api.SearchParameters
+import org.springframework.social.twitter.api.Tweet
+import org.springframework.social.twitter.api.impl.TwitterTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class TwitterService {
+    val twitter = TwitterTemplate("qPe0o5axovP4VlGnv9bDchVwI", "yFWHo5mHwIy99jlTfgp2loSpnRlGLMwRvpNJqqFC6NQQfqal15")
+
+    fun getHelloWorldTweets(number: Int): Flowable<Tweet> {
+        return Flowable.fromIterable(this.twitter.searchOperations().search(
+                SearchParameters("hello world").count(number)
+        ).tweets)
+    }
 }
