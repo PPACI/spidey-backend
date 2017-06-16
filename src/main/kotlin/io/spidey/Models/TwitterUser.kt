c21e9654e82e1d4385bdded390978b0044920b90
Stéphane <ralys@gmx.fr>
2017-06-17 00:07:35 +0200

    fix(bannerPictureUrl nullable)

--- src/main/kotlin/io/spidey/Models/TwitterUser.kt
+++ src/main/kotlin/io/spidey/Models/TwitterUser.kt
@@ -4,5 +4,5 @@ data class TwitterUser(
     val userName: String,
     val description: String,
     val profilePictureUrl: String,
-    val bannerPictureUrl: String
+    val bannerPictureUrl: String?
 )
