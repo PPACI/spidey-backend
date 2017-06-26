20650d48c4eecb4c18927d794cf9721cc9441e17
pierre paci <pierre.paci@gmail.com>
2017-06-26 20:53:41 +0200

    now retrieve tweets from cache

--- src/main/kotlin/io/spidey/Services/RelationService.kt
+++ src/main/kotlin/io/spidey/Services/RelationService.kt
@@ -2,16 +2,28 @@ package io.spidey.Services
 
 import io.reactivex.Observable
 import io.spidey.repository.TweetRepository
+import io.spidey.repository.TwitterUserRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 
 @Service
 class RelationService(val twitterService: TwitterService, val tweetRepository: TweetRepository) {
     fun getPairsOfRelation(screenName: String): Observable<Pair<String, String>> {
-        //TODO: IMPLEMENT CACHE METHOD HERE
-        return Observable.fromIterable(this.twitterService.getUserTimelineForScreenName(screenName))
-                .map { it.retweetedStatus?.fromUser ?: it.inReplyToScreenName }
-                .distinct()
-                .map { Pair(screenName, it) }
+        val cacheTweets = this.tweetRepository.findByfromUser(screenName)
+        val lastFetchedAt = cacheTweets.sortedByDescending { it.fetchedAt }[0].fetchedAt
+        if (cacheTweets.isNotEmpty() && lastFetchedAt != null && Date().time - lastFetchedAt.time < 604800000) { // 1 week
+            return Observable.fromIterable(cacheTweets)
+                    .map { it.retweedFromScreenName ?: it.replyToScreenName ?: "" }
+                    .filter { it != "" }
+                    .distinct()
+                    .map { Pair(screenName, it) }
+        } else {
+            return Observable.fromIterable(this.twitterService.getUserTimelineForScreenName(screenName))
+                    .map { it.retweetedStatus?.fromUser ?: it.inReplyToScreenName }
+                    .distinct()
+                    .map { Pair(screenName, it) }
+            //TODO: add Tweets to cache
+        }
     }
 }
