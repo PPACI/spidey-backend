d1e0af547b84892d242b526f3b78f963b7dc7f2c
pierre paci <pierre.paci@gmail.com>
2017-06-26 20:46:11 +0200

    Tweet now have a field to know when they were scraped

--- src/main/kotlin/io/spidey/Models/Tweet.kt
+++ src/main/kotlin/io/spidey/Models/Tweet.kt
@@ -2,11 +2,14 @@ package io.spidey.Models
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
+import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
 import java.util.*
 
 @Document(indexName = "twitter", type = "tweet")
-class Tweet(@Id val id: String,
-            val createdAt: Date,
-            val fromUser: String,
-            val replyToScreenName: String,
-            val retweedFromScreenName: String)
+class Tweet(@Id val id: String? = null,
+            val fromUser: String? = null,
+            val replyToScreenName: String? = null,
+            val retweedFromScreenName: String? = null,
+            @Field(type = FieldType.Date) val createdAt: Date? = null,
+            @Field(type = FieldType.Date) val fetchedAt: Date? = null)
