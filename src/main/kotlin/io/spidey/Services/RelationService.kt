b1f126ab87ff11b5e1c7da036d4dccf43b7ebbbb
pierre paci <pierre.paci@gmail.com>
2017-06-27 20:22:53 +0200

    added code to store tweet in cache

--- src/main/kotlin/io/spidey/Services/RelationService.kt
+++ src/main/kotlin/io/spidey/Services/RelationService.kt
@@ -1,6 +1,7 @@
 package io.spidey.Services
 
 import io.reactivex.Observable
+import io.spidey.Models.Tweet
 import io.spidey.repository.TweetRepository
 import io.spidey.repository.TwitterUserRepository
 import org.springframework.stereotype.Service
@@ -11,19 +12,32 @@ import java.util.*
 class RelationService(val twitterService: TwitterService, val tweetRepository: TweetRepository) {
     fun getPairsOfRelation(screenName: String): Observable<Pair<String, String>> {
         val cacheTweets = this.tweetRepository.findByfromUser(screenName)
-        val lastFetchedAt = cacheTweets.sortedByDescending { it.fetchedAt }[0].fetchedAt
-        if (cacheTweets.isNotEmpty() && lastFetchedAt != null && Date().time - lastFetchedAt.time < 604800000) { // 1 week
+        val lastFetchedAt = cacheTweets.sortedByDescending { it.fetchedAt }.elementAtOrNull(0)?.fetchedAt
+        if (lastFetchedAt != null && Date().time - lastFetchedAt.time < 604800000) { // 1 week
             return Observable.fromIterable(cacheTweets)
                     .map { it.retweedFromScreenName ?: it.replyToScreenName ?: "" }
                     .filter { it != "" }
                     .distinct()
                     .map { Pair(screenName, it) }
         } else {
+            val creationDate = Date()
             return Observable.fromIterable(this.twitterService.getUserTimelineForScreenName(screenName))
-                    .map { it.retweetedStatus?.fromUser ?: it.inReplyToScreenName }
+                    .filter { it.retweetedStatus != null || it.inReplyToScreenName != null }
+                    .doOnNext {
+                        this.tweetRepository.save(
+                                Tweet(
+                                        id = it.id,
+                                        fromUser = it.fromUser,
+                                        createdAt = it.createdAt,
+                                        replyToScreenName = it.inReplyToScreenName,
+                                        retweedFromScreenName = it.retweetedStatus?.fromUser,
+                                        fetchedAt = creationDate
+                                ))
+                    }
+                    .map { it.retweetedStatus?.fromUser ?: it.inReplyToScreenName ?: "" }
+                    .filter { it != "" }
                     .distinct()
                     .map { Pair(screenName, it) }
-            //TODO: add Tweets to cache
         }
     }
 }
